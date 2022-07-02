d836661f596e9d12d6bbe37b69c02d640f179c94
Abha78 <abhatanisha78@gmail.com>
2022-07-02 15:41:00 +0530

    My Files

---
+++ Half_pyramid.java
@@ -0,0 +1,22 @@
+/*
+ 
+*
+**
+***
+****
+
+ */
+import java.util.*;
+public class Half_pyramid{
+    public  static void main(String arg[]){
+        int n =4;
+        for(int i =1 ; i<=4 ; i++)
+        {
+        for(int j =1 ; j<=i ; j++)
+{
+    System.out.print("*");
+}
+System.out.println(" ");
+}
+    }
+} 
---
+++ Solid_rectangle.java
@@ -0,0 +1,22 @@
+/*
+ *****
+ *****
+ *****
+ *****
+
+ */
+import java.util.*;
+public class Solid_rectangle{
+    public static void main(String[] args) {
+        int n = 4;
+        int m= 5;
+        Scanner sc = new Scanner(System.in);
+        for(int i = 1; i<=n; i++){
+            for(int j =1 ; j<=m ; j++){
+
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
